7dc0aedfb63ca824d05a9e82ad8bb08a19a29412
Mikhail <mb.38@yandex.ru>
2020-08-27 10:09:04 +0700

    Updates

---
+++ precache-manifest.d37c4c8224158f82b9530729dc9a5bac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cdb255d87d864166934f585484025ca",
+    "url": "/Github-search-react/index.html"
+  },
+  {
+    "revision": "ae85ccd696c2d55781c8",
+    "url": "/Github-search-react/static/css/main.2e643e74.chunk.css"
+  },
+  {
+    "revision": "1baca8045d42278bd6c5",
+    "url": "/Github-search-react/static/js/2.f6a89fe6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Github-search-react/static/js/2.f6a89fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae85ccd696c2d55781c8",
+    "url": "/Github-search-react/static/js/main.3cbaa6ac.chunk.js"
+  },
+  {
+    "revision": "efa732c37541a9dfcc60",
+    "url": "/Github-search-react/static/js/runtime-main.05b28f17.js"
+  }
+]);
